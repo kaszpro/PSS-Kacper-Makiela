0fdd5b85a8ea84b21891d84b62c35d585e762fc2
Kacper_M_INFIII <75495256+kaszpro@users.noreply.github.com>
2024-11-28 17:28:25 +0100

    Delete php kontroler.php

--- php kontroler.php
+++
@@ -1,8 +0,0 @@
-<?php
-// KONTROLER strony kalkulatora kredytowego
-require_once dirname(__FILE__).'/../config.php';
-
-// Pobranie parametrów z formularza
-$amount = $_REQUEST['amount']; // Kwota kredytu
-$years = $_REQUEST['years'];   // Okres spłaty w latach
-$interest = $_REQUEST['interest']; // Oprocentowanie
